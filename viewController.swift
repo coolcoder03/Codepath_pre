63b900e6f035252a0f0fd746ed8f61ae37fdefeb
zeba wahab <zeba1506@gmail.com>
2023-08-27 16:02:41 -0700

    Update viewController.swift

--- viewController.swift
+++ viewController.swift
@@ -6,18 +6,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var universityLabel: UILabel!
     @IBOutlet weak var jobLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        let randomColor = UIColor(
-            red: CGFloat.random(in: 0...1),
-            green: CGFloat.random(in: 0...1),
-            blue: CGFloat.random(in: 0...1),
-            alpha: 1.0
-        )
+        let randomColor = changeColor()
         backgroundView.backgroundColor = randomColor
+    }
+
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
